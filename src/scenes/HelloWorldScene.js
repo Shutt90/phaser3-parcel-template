86d536171a990c645c3c70e05a80caf6c6df6509
Liam Pugh <78647249+Shutt90@users.noreply.github.com>
2021-09-06 17:05:33 +0100

    First

--- src/scenes/HelloWorldScene.js
+++ src/scenes/HelloWorldScene.js
@@ -4,36 +4,28 @@ export default class HelloWorldScene extends Phaser.Scene
 {
 	constructor()
 	{
-		super('hello-world')
+		super()
 	}
 
 	preload()
     {
-        this.load.setBaseURL('http://labs.phaser.io')
-
-        this.load.image('sky', 'assets/skies/space3.png')
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png')
+        this.load.image('coin', 'assets/items_trap_animations/coin/coin_1.png')
+        this.load.image('logo', 'assets/items_trap_animations/coin/coin_2.png')
+        
         this.load.image('red', 'assets/particles/red.png')
     }
 
     create()
     {
-        this.add.image(400, 300, 'sky')
-
-        const particles = this.add.particles('red')
-
-        const emitter = particles.createEmitter({
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        })
+        this.coin = this.add.image(400, 300, 'coin')
+        this.coin.setScale(1.5).phys
 
         const logo = this.physics.add.image(400, 100, 'logo')
+        logo.setScale(2.0);
 
         logo.setVelocity(100, 200)
         logo.setBounce(1, 1)
         logo.setCollideWorldBounds(true)
 
-        emitter.startFollow(logo)
     }
 }
